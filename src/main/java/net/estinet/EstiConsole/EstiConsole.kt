a5adce38b0b9845194ce5518e3e4fb7411a1a08b
_InDev_ <aguy867@gmail.com>
2017-03-13 10:40:57 -0400

    Switched socket.io clients

--- src/main/java/net/estinet/EstiConsole/EstiConsole.kt
+++ src/main/java/net/estinet/EstiConsole/EstiConsole.kt
@@ -14,7 +14,7 @@ import java.nio.file.Files
 import java.util.*
 
 object EstiConsole {
-    var version: String = "v1.1.0"
+    var version: String = "v1.1.1"
     lateinit var javaProcess: Process
     lateinit var writer: PrintWriter
     var autoStartOnStop = false

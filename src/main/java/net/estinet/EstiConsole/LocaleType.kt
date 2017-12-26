d42a367d260efbb0560c00a7570f8fd85fede335
espidev <aguy867@gmail.com>
2017-12-26 11:17:46 -0500

    Switched to golang code base

--- src/main/java/net/estinet/EstiConsole/LocaleType.kt
+++
@@ -1,5 +0,0 @@
-package net.estinet.EstiConsole
-
-enum class LocaleType{
-    UNKNOWN_COMMAND, PREFIX, DISABLING, DISABLED, ENABLING, ENABLED, ERR_ON_START
-}
--- src/main/java/net/estinet/EstiConsole/Modes.kt
+++
@@ -1,5 +0,0 @@
-package net.estinet.EstiConsole
-
-enum class Modes{
-    PAPERSPIGOT, SPIGOT, BUNGEE
-}
